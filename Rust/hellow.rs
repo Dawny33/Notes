8891f2c6359c56fb4d2f40c9a3a2835ea052a687
Jalem Raj Rohit <jrajrohit33@gmail.com>
2018-01-23 21:37:10 +0530

    Started Rust

---
+++ Rust/hellow.rs
@@ -0,0 +1,8 @@
+
+// This is a comment
+// Generate the binary of this program by doing `rustc hellow.rs`
+// Then execute the binary as: `./hellow`
+
+fn main() {
+	println!("Hello World!");
+}
---
+++ Rust/variables.rs
@@ -0,0 +1,10 @@
+fn main() {
+	// This is an inline comment
+
+	/*
+	 This is a block comment
+	*/
+
+	let x = 5 + 5;
+	println!("x is {}", x);
+}
