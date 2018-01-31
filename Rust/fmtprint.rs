4ca06de55aa13447d80229773a632d2036c08440
Jalem Raj Rohit <jrajrohit33@gmail.com>
2018-01-31 22:25:48 +0530

    Formatted print completed

--- Rust/fmtprint.rs
+++ Rust/fmtprint.rs
@@ -9,8 +9,16 @@ fn main() {
 	println!("{} out of {:2} people know about binary. The others doesn't", 1, 2);
 
 	// Adding spaces before a number. This adds 5 spaces before 1
-	println!("{number:>width$}", number=1, width=6)
+	println!("{number:>width$}", number=1, width=6);
 
 	// Adding padding with other numbers. This will print 000001 (five zeroes and a 1)
-	println!("{number:>width$}")
-}	
+	println!("{number:>0width$}", number=1, width=6);
+
+	// Creating a structure which consists i32 and naming it Structure
+	#[allow(dead_code)]
+	struct Structure(i32);
+
+	// Simply trying to print structure using std:fmt doesn't work
+	// println!('This does not work: {}', Structure(i32));
+
+}
